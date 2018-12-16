e8e36f223d43d4fd0a6d161cee4de3418f7a3945
Orlando <Orlando@ARES>
2018-12-16 23:03:48 +0100

    First Commit

---
+++ receipt-project/src/tableOrder.java
@@ -0,0 +1,13 @@
+
+public class tableOrder {
+
+	public static int getTableNumber() {
+		
+		String tableNumber;
+		tableNumber=Utilities.getUserIntorSpecificInput(MSG_TABLE_NUMBER, Q, 1, MAX_TABLE_NUMBER);
+		if(tableNumber.equalsIgnoreCase(Q)) {
+			System.exit(0);
+		}
+		return Integer.parseInt(tableNumber);
+	}
+}
